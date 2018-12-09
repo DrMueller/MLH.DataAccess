c7504045f9cac385fe7d44b87c1f09412bbe490f
Matthias Mueller <matthiasm@live.de>
2018-12-09 17:09:17 +0100

    WIP

--- Sources/Application/Areas/DataModeling/Models/DataModelbase.cs
+++ Sources/Application/Areas/DataModeling/Models/DataModelbase.cs
@@ -8,5 +8,52 @@
     {
         public string DataModelTypeName => GetType().FullName;
         public TId Id { get; set; }
+
+        public static bool operator ==(DataModelBase<TId> a, DataModelBase<TId> b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+            {
+                return false;
+            }
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(DataModelBase<TId> a, DataModelBase<TId> b)
+        {
+            return !(a == b);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as DataModelBase<TId>;
+
+            if (ReferenceEquals(compareTo, null))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, compareTo))
+            {
+                return true;
+            }
+
+            if (GetType() != compareTo.GetType())
+            {
+                return false;
+            }
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType() + Id.ToString()).GetHashCode();
+        }
     }
 }

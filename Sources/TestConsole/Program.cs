6d4d2f77241edfacd451ec6a815a986c48fff55c
Matthias Müller <matthias.mueller@trivadis.com>
2022-08-11 09:53:57 +0200

    WIP

---
+++ Sources/TestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mmu.Mlh.DataAccess.TestConsole
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.ReadKey();
+        }
+    }
+}
